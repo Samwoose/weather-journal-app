d9bb8cde0e87f97505006402668e568403cc8b79
Samwoo Seong <tjdtkadn@gmail.com>
2021-08-04 23:51:26 +0900

    Create a server running on a port with Express

--- server.js
+++ server.js
@@ -2,18 +2,32 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+//local host port is 3000
+const port = 3000;
+//These lines of code will verify if the server is running by 
+//calling callback function
+const server = app.listen(port, callback);
+function callback(){
+    console.log('server running');
+    console.log(`runnig on localhost: ${port}`);
+}
